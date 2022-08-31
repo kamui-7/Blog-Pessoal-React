88812c255912854d4d1ec1d9b0c6958ee35168cb
kamui-7 <gabrielmarinsrodrigues@gmail.com>
2022-08-31 10:53:33 -0300

    Adcionado o primeiro componente ao Projeto

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://www.wallpaperuse.com/wallp/46-465161_m.jpg" alt="Imagem Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
