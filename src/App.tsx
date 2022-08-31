408d877c567882d8d31509480cfdb30f6fd83a96
kamui-7 <gabrielmarinsrodrigues@gmail.com>
2022-08-31 15:19:42 -0300

    (Adcionado componentes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import { Grid } from '@material-ui/core';
 import './App.css';
 import Home from "./paginas/home/Home";
 
 
+
 function App() {
   return (
-   <Home />
+    <>
+    <Navbar />
+    <Home />
+    <Footer />
+    </>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import {Button, Box, Paper} from "@material-ui/core";
 import "./Home.css";
+
 
 function Home(){
     return(
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://www.wallpaperuse.com/wallp/46-465161_m.jpg" alt="Imagem Inicial" className="img"/>
+        <Paper>
+            <Box p={2}>
+                <Box display="flex" justifyContent="center">
+                    <h1>Titulo</h1>
+                </Box>
+                    <img src="https://images.gamebanana.com/img/ico/sprays/pokeball.png" alt="Pokebola" style={{height: "100%", width: "100%"}} />
+                <Box display="flex" justifyContent="center" p={2}>
+                        <Button variant="contained" color="primary"> Texto 1 </Button>
+                        <Button variant="contained" color="secondary"> Texto 2 </Button>
+                    </Box>
+            </Box>
+        </Paper>
         </>
     );
 }
