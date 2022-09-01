f4a797b7632690527c9711dfa264eef99d2e325b
kamui-7 <gabrielmarinsrodrigues@gmail.com>
2022-09-01 14:44:50 -0300

    Login Criado e Rotas Adcionadas[01/09/2022]

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import './App.css';
 import Home from "./paginas/home/Home";
-
+import Login from "./paginas/login/Login";
+import CadastroUsuario from "./paginas/cadastrousuario/CadastroUsuario";
 
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Home />
-    <Footer />
-    </>
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cadastro" element={<CadastroUsuario />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/paginas/cadastrousuario/CadastroUsuario.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import { Box } from "@mui/material";
+import './CadastroUsuario.css';
+
+function CadastroUsuario() {
+
+    return (
+        <>
+        </>
+);
+}
+
+export default CadastroUsuario;
